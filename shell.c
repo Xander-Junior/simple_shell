72978947669ff5fa31159ce7d31c830b0017b8c5
Xander-Junior <xanderjnr278@gmail.com>
2023-10-18 18:29:03 -0700

    Test task 0

--- shell.c
+++ shell.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #define INPUT_SIZE 1024
 
@@ -8,11 +9,19 @@ int main()
 {
 	char *input = NULL;
 	size_t len = 0;
+	char *argv[2] = {NULL, NULL};
+	char *envp[] = {NULL};
 
 	while (1)
 	{
 		printf("$ ");
-		getline(&input, &len, stdin);
+		ssize_t read = getline(&input, &len, stdin);
+		if (read == -1)
+		{
+			printf("\n");
+			free(input);
+			exit(0);
+		}
 
 		size_t input_length = strlen(input);
 		if (input_length > 0 && input[input_length - 1] == '\n')
@@ -23,6 +32,12 @@ int main()
 		{
 			free(input);
 			exit(0);
+		}
+
+		argv[0] = input;
+		if (execve(argv[0], argv, envp) == -1)
+		{
+			printf("./shell: No such file or directory\n");
 		}
 	}
 	return (0);
